23548f9b5a1c397f2fc2a471e354787001d57191
Jadelynn Cheatum <cheatumj@proton.me>
2024-10-04 23:25:39 -0500

    Add health bar to ant

---
+++ scripts/components/health_bar.gd
@@ -0,0 +1,12 @@
+extends ProgressBar
+
+signal empty
+
+
+func damage(value: float) -> void:
+	self.value -= value
+
+
+func _on_value_changed(value: float) -> void:
+	if value == 0:
+		empty.emit()
