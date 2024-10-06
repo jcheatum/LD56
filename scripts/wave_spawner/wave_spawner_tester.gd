9794a26f4344a7fefe1227f63ae614006b2921d8
Jake Barnes <jakebarnescodes@gmail.com>
2024-10-05 19:16:07 -0500

    Wave Spawner

---
+++ scripts/wave_spawner/wave_spawner_tester.gd
@@ -0,0 +1,19 @@
+extends Node
+
+#just for testing purposes
+
+@onready var spawner: Area2D = $"../WaveSpawner"
+
+var wave_num: int = 0
+
+func _ready() -> void:
+	spawner.wave_done.connect(_on_wave_done)
+	await spawner.ready
+	spawner.start_wave(wave_num)
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_wave_done():
+	wave_num += 1
+	spawner.start_wave(wave_num)
