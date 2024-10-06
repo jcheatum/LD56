c1b1ed28fd25f1ec5d48ae7e0cbdff062037eeff
Jadelynn Cheatum <cheatumj@proton.me>
2024-10-06 18:33:50 -0500

    Make spray only spraying when bugs are in it

--- scripts/towers/tower_spray.gd
+++ scripts/towers/tower_spray.gd
@@ -7,4 +7,10 @@ func ACTIVE_UPDATE(delta):
 	if FlameZone.has_overlapping_areas():
 		for collider in FlameZone.get_overlapping_areas():
 			if collider != null and collider.has_method("damage"):
+				$AnimatedSprite2D.frame = 1
 				collider.damage(damage*delta)
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is Bug:
+		$AnimatedSprite2D.frame = 0
