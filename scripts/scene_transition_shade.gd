7cfc4074c90ace07df235779b6f29bef1bb72542
Jadelynn Cheatum <cheatumj@proton.me>
2024-10-07 04:37:11 -0500

    Create scene transition shade

---
+++ scripts/scene_transition_shade.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+signal entrance_finished
+signal exit_finished
+
+
+func enter() -> void:
+	$AnimationPlayer.play("enter")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "enter":
+		entrance_finished.emit()
+	elif anim_name == "exit":
+		exit_finished.emit()
